4075fa99bf47c9e3b8082f64af28ac798911b1a3
Shin <mrmaxin@gmail.com>
2016-03-04 14:03:51 -0800

    Submit 4.6.1

--- week-4/smallest-integer/my_solution.rb
+++ week-4/smallest-integer/my_solution.rb
@@ -10,33 +10,37 @@
 #
 # If +list_of_nums+ is empty the method should return nil
 
-# Your Solution Below
+######################## Your Solution Below#####################
+# def smallest_integer(list_of_nums)
+#   if list_of_nums == nil #test to see if array is empty, if so return nil
+#   	return nil
+#   elsif list_of_nums.length == 1 #test to see if array is only 1 index long
+#   	return list_of_nums[0]
+#   end
+
+#   counter = 0
+#   smallest_num = list_of_nums[0]
+#   # smaller_list = list_of_nums[1..-1]
+#   while counter < list_of_nums.length #go through every member of the array with a loop interation
+#     if smallest_num >= list_of_nums[counter] # perform logic test by comparing items
+#       smallest_num = list_of_nums[counter] # change the compared variable to the smallest if logic passes 
+#       #p smallest_num to test
+#     end
+#     counter += 1 # compare to each in the remaining array
+#   end
+
+#   return smallest_num
+# end
+
+################################# Refactor ####################
 def smallest_integer(list_of_nums)
   if list_of_nums == nil #test to see if array is empty, if so return nil
-  	return nil
-  elsif list_of_nums == [0]
-  	return 0
-  elsif list_of_nums.length == 1
-  	return list_of_nums[0]
+    return nil
+  elsif list_of_nums.length == 1 #test to see if array is only 1 index long
+    return list_of_nums[0]
+  else
+    return list_of_nums.sort.first
   end
-
-  counter = 0
-  smallest_num = list_of_nums[0]
-  smaller_list = list_of_nums[1..-1]
-  while smaller_list.length != 0 #go through every member of the array with a loop interation
-  	
-  end
-	# if smallest_num < smaller_list[0]
-	# 	smaller_list = smallest_list[1..-1]
-	# elsif smallest_num > smaller_list[0]
-	# 	smallest_num = smallest_list[0]
-	# 	return smallest_num
-	# else
-	# 	return 0
-	# end
-	# 	 # perform logic test by comparing items
- #  	# pop out first item and save to a variable A and compare to each in the remaining array
- #  	# if the variable A is smaller than the first item in the array, move to the next item in the array
- #  	# if the the item in the array is smaller, assign variable A to the item
-
 end
+
+	

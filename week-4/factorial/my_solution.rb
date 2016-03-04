5f9ca78234d54c8a358e01006a657a9ccc42e566
Shin <mrmaxin@gmail.com>
2016-03-03 23:20:11 -0800

    Update 4.7.1

--- week-4/factorial/my_solution.rb
+++ week-4/factorial/my_solution.rb
@@ -1,16 +1,30 @@
-def factorial(number) #input is a integer
-  if number == 0 || number == 1 #if number is 0 or 1, output 1
-    return 1
-  end
-  
-  product = number #initiate variable and assign it to the input
-  while number > 1 #loop from number until it counts down to > 0
-    product = product * (number - 1)#if number = 2, then product = number * number - 1
+# def factorial(number) #input is a integer
+#   if number == 0 || number == 1 #if number is 0 or 1, output 1
+#     return 1
+#   else
+#   	product = number #initiate variable and assign it to the input
+#   	while number > 1 #loop from number until it counts down to > 0
+#     	product = product * (number - 1)#if number = 2, then product = number * number - 1
     
-    number -= 1#number = number minus 1
-   
-  end
+#     	number -= 1#number = number minus 1
+# 	end
+#   end
   
-  return product #output
+#   return product #output
   
+# end
+
+
+def factorial(number)
+	if zero_and_one(number) == true
+		return 1
+	else
+		product = number *factorial(number-1)
+	end
+end
+
+def zero_and_one(number)
+	if number == 0 || number == 1
+		return true
+	end
 end

f416c286d7ce60a812fb7ee97ec1f22f414b56a0
Shin <mrmaxin@gmail.com>
2016-03-14 16:28:27 -0700

    Try out refactor answer from peer

--- week-5/acct_groups_test.rb
+++ week-5/acct_groups_test.rb
@@ -1,4 +1,5 @@
 #### Solution 2 with hashes
+=begin
 def group_divider (array_of_names)
   if array_of_names == nil
     p "There's no one here."
@@ -59,3 +60,123 @@ end
 def sort_by_group_num (list, key, value)
 
 end
+=end
+#########################
+# def grouper(names) # Gives different groups each time
+#     i = 1
+#     hash = {}
+#     until names.length == 0
+#       group = names.sample(3)
+#       hash[group] = i
+#       names = names - group
+#       i += 1
+#     end
+#   if group.length < 2
+#     hash.key(1) << group.join
+#   end
+#   hash.delete(group)
+#   hash.each do |key,value|
+#       puts "Group #{value} : #{key}"
+#     end
+# end
+
+
+# def grouper(names) # Gives the same groups each time
+#   i=0
+#   j=2
+#   k=1
+#   hash = {}
+#     until names.length == 0
+#     group = names[i..j]
+#     hash[group] = k
+#     names.slice!(i..j)
+#     k+=1
+#     end
+#   if group.length < 2
+#     hash.key(1).push(group.join)
+#   end
+#   hash.delete(group)
+#   hash.each do |key,value|
+#     puts "Group #{value} : #{key}"
+#   end
+# end
+#################################
+def grouper(list)
+  new_array = []
+  while list.count != 0
+    group = list.sample(4)
+    new_array.push group
+    for student in group
+     list.delete(student)
+    end
+   end
+  p new_array
+end
+
+students = ["Aarthi Gurusami", "Abid Ramay",
+"Adam Zmudzinski",
+"Alec Hendrickson",
+"Alex Wen",
+"Alicia Briceland",
+"Allison Paul",
+"Andrey Slonski",
+"Anna Lansfjord",
+"Ben Flores",
+"Ben Sanecki",
+"Bill Deng",
+"Buck Melton",
+"Caitlin Hoffman",
+"Carlos Gonzalez",
+"Chand Nirankari",
+"Chris Henderson",
+"Christopher Lamkin",
+"Christyn Budzyna",
+"Ché Sanders",
+"Dan Park",
+"David Ramirez",
+"David Tao",
+"David Walden",
+"Denny Jovic",
+"Dexter Moran",
+"Diana Ozemebhoya Eromosele",
+"Dominick Lombardo",
+"Elan Kvitko",
+"Elizabeth Alexander",
+"Elizabeth Brown",
+"Ena Bek",
+"Esther Leytush",
+"Evan Druce",
+"Frank Lam",
+"Gabriel Zurita",
+"Jack Thatcher",
+"Jason Milfred",
+"John Colella",
+"Jonathan Kaplan",
+"Kelson Adams",
+"Kristal Lam",
+"Kunal Patel",
+"Leland Meiners",
+"Liam Binell",
+"Lisa Buch",
+"Lisa Dannewitz",
+"Lyudmila Arinich",
+"Mohamed Monekata",
+"Parker Smathers",
+"Patrick DeWitte",
+"Renan Martins",
+"Riley Scheid",
+"Robin Soubry",
+"Samantha Holmes",
+"Scott Southard",
+"Shaun R Sweet",
+"Shin Wang",
+"Sibel Ergener",
+"Simon Thomas",
+"Talal Talhouk",
+"Ted Bogin",
+"Traci Fong",
+"Victoria Solorzano",
+"one more"]
+
+acct_groups = grouper(students)
+#p acct_groups.invert[3]

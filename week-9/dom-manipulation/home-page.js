df86b22b730f255925d85b0042bb162615d2014b
Shin <mrmaxin@gmail.com>
2016-04-06 20:21:21 -0700

    Update 9.3

--- week-9/dom-manipulation/home-page.js
+++ week-9/dom-manipulation/home-page.js
@@ -22,7 +22,8 @@ var x = document.getElementsByTagName("H1");
 x[0].innerHTML = "I completed release 2.";
 
 // Release 4:
-document.getElementById("release-3").style.backgroundColor = "#955251";
+var release4 = document.getElementById("release-3");
+release4.style.backgroundColor = "#955251";
 
 // Release 5:
 var release5 = document.getElementsByClassName("release-4");
@@ -33,5 +34,11 @@ for (var i in release5){
 // var tmpl = document.querySelector('#hidden');
 // var clone = tmpl.content.cloneNode(true);
 // tmpl.parentNode.appendChild(clone);
+
 var tmpl = document.getElementById("hidden");
 document.body.appendChild(tmpl.content.cloneNode(true));
+
+// var theParagraph = document.getElementById("hidden");
+// var theCopy = theParagraph.cloneNode(true);
+// var theBody = document.getElementsByTagName("body");
+// theBody[0].appendChild(theCopy);
